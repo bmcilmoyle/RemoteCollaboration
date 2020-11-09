72286496620a3ece723645f60e7ea950286a51d0
DESKTOP-VLR7K02\ben.mcilmoyle <ben.mcilmoyle@unity3d.com>
2020-11-09 13:12:58 -0500

    Init Commit, basic scenes setup

---
+++ Assets/Scripts/AdditiveSceneLoading.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Scene Management required
+using UnityEngine.SceneManagement;
+
+public class AdditiveSceneLoading : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Cubes", LoadSceneMode.Additive);
+            SceneManager.LoadScene("Capsules", LoadSceneMode.Additive);
+        }
+    }
+}
---
+++ Assets/Scripts/PrefabLoading.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabLoading : MonoBehaviour
+{
+    public List<GameObject> prefabsToLoad;
+
+    void Start()
+    {
+
+
+        //Load each prefab in the game
+        foreach (GameObject prefab in prefabsToLoad)
+        {
+            Instantiate(prefab);
+        }
+    }
+}
